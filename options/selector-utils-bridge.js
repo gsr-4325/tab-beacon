d07605612887bad4726863248d6fc90eeed9b9d1
gsr-4325 <252289774+gsr-4325@users.noreply.github.com>
2026-04-09 12:54:37 +0900

    refactor: refresh selector bridge for dynamic option inputs

--- options/selector-utils-bridge.js
+++ options/selector-utils-bridge.js
@@ -8,6 +8,12 @@
     ? window.resolveSelectorType.bind(window)
     : null;
 
+  const refreshSelectorInputs = (root = document) => {
+    for (const input of root.querySelectorAll(".condition-query, .scope-query")) {
+      input.dispatchEvent(new Event("input", { bubbles: true }));
+    }
+  };
+
   window.resolveSelectorType = function resolveSelectorTypeBridge(query, selectorType) {
     try {
       return shared.resolveSelectorType(query, selectorType, document);
@@ -20,7 +26,29 @@
     }
   };
 
-  for (const input of document.querySelectorAll(".condition-query, .scope-query")) {
-    input.dispatchEvent(new Event("input", { bubbles: true }));
+  refreshSelectorInputs(document);
+
+  const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        if (!(node instanceof Element)) continue;
+        if (node.matches?.(".condition-query, .scope-query")) {
+          node.dispatchEvent(new Event("input", { bubbles: true }));
+          continue;
+        }
+        refreshSelectorInputs(node);
+      }
+    }
+  });
+
+  if (document.body) {
+    observer.observe(document.body, { childList: true, subtree: true });
+  } else {
+    window.addEventListener("DOMContentLoaded", () => {
+      if (document.body) {
+        observer.observe(document.body, { childList: true, subtree: true });
+      }
+      refreshSelectorInputs(document);
+    }, { once: true });
   }
 })();
